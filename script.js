e1eba48868b354fa88fb505ef93d18afe3669efc
James Lopez <lopez.jamesrussel@gmail.com>
2022-01-10 05:24:27 -0500

    Add Column Functionality

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ $(document).ready(function () {
 
     $('.add-row').click(function () {
         $('.row').last().clone().appendTo('.container')
+        //$('.row').last().addClass("last")
     })
 
     $('.del-row').click(function () {
         $('.row').last().remove()
     })
 
+    $('.add-col').click(function () {
+        $('.col-sm').last().clone().appendTo('.row')
+    })
 })
