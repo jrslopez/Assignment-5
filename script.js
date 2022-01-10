6900ea61a48a100336e637dcdd0bedff89a9f7b0
James Lopez <lopez.jamesrussel@gmail.com>
2022-01-10 07:45:59 -0500

    Cleaned up code

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-//console.log("Hello from script")
-
 $(document).ready(function () {
 
     $('.add-row').click(function () {
         $('.row').last().clone().appendTo('.container')
-        //$('.row').last().addClass("last")
     })
 
     $('.del-row').click(function () {
@@ -13,7 +10,6 @@ $(document).ready(function () {
 
     $('.add-col').click(function () {
         $('.col-sm').last().clone().appendTo('.row')
-        //$('div.col-sm:last-child').clone().appendTo('.row')
     })
 
     $('.del-col').click(function () {
@@ -22,7 +18,6 @@ $(document).ready(function () {
 
     $('.changeAll').click(function () {
         let color = $('#changeAll').val()
-        //console.log(color)
         $('.row').children().each(function () {
             $(this).css('background-color', color)
         })
@@ -31,10 +26,8 @@ $(document).ready(function () {
     $('.changeWhite').click(function () {
         let color = $('#changeWhite').val()
         $('.row').children().each(function (){
-            //console.log($(this).css('background-color'))
             if ($(this).css('background-color') == 'rgba(0, 0, 0, 0)')
             {
-                //console.log("success")
                 $(this).css('background-color', color)
             }
         })
